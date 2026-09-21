1848f867d61a7bada79b7a7fec833fcc3d984fae
Floriano valadares <florianovaladares11@gmail.com>
2026-09-21 02:16:38 -0300

    Refactor tic-tac-toe game logic and interface

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,99 +1,78 @@
-import random
+def interface():
+    print("   0   1   2")
+    print("0 [{}] [{}] [{}]".format(tabuleiro[0][0],tabuleiro[0][1],tabuleiro[0][2]))
+    print("1 [{}] [{}] [{}]".format(tabuleiro[1][0],tabuleiro[1][1],tabuleiro[1][2]))
+    print("2 [{}] [{}] [{}]".format(tabuleiro[2][0],tabuleiro[2][1],tabuleiro[2][2]))
 
-def jogo_da_velha_inteligente():
-    tabuleiro = [" " for _ in range(9)]
+def validarVitoria(rodada):
+    global parar    
+    if(tabuleiro[0][0] == rodada and tabuleiro[0][1] == rodada and tabuleiro[0][2] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True
+
+    if(tabuleiro[1][0] == rodada and tabuleiro[1][1] == rodada and tabuleiro[1][2] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True     
+
+    if(tabuleiro[2][0] == rodada and tabuleiro[2][1] == rodada and tabuleiro[2][2] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True
+
+    if(tabuleiro[0][0] == rodada and tabuleiro[1][0] == rodada and tabuleiro[2][0] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True
+        
+    if(tabuleiro[0][1] == rodada and tabuleiro[1][1] == rodada and tabuleiro[2][1] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True
+        
+    if(tabuleiro[0][2] == rodada and tabuleiro[1][2] == rodada and tabuleiro[2][2] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True
+        
+    if(tabuleiro[0][0] == rodada and tabuleiro[1][1] == rodada and tabuleiro[2][2] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True
+
+    if(tabuleiro[2][0] == rodada and tabuleiro[1][1] == rodada and tabuleiro[0][2] == rodada):
+        interface()
+        print("O jogador {} venceu!".format(rodada))
+        parar = True
+            
+tabuleiro = [[" "," "," "],[" "," "," "],[" "," "," "]]
+
+parar = False
+rodada = "X"
+Jogadas = 0
+
+while parar == False:
+    if Jogadas == 9:
+            interface()
+            print("Empate!")
+            parar = True
     
-    def mostrar_tabuleiro():
-        print()
-        print(f"{tabuleiro[0]} | {tabuleiro[1]} | {tabuleiro[2]}")
-        print("--+---+--")
-        print(f"{tabuleiro[3]} | {tabuleiro[4]} | {tabuleiro[5]}")
-        print("--+---+--")
-        print(f"{tabuleiro[6]} | {tabuleiro[7]} | {tabuleiro[8]}")
-        print()
+    interface()
+
+    linha = int(input("Digite a linha escolhida:"))
+    coluna = int(input("Digite a coluna escolhida:"))
+
+    if rodada == "X":
+        tabuleiro[linha][coluna] = "X"
+        validarVitoria(rodada)
+        Jogadas += 1
+        rodada = "0"
     
-    def checar_vencedor(jogador):
-        combinacoes = [
-            [0,1,2], [3,4,5], [6,7,8],
-            [0,3,6], [1,4,7], [2,5,8],
-            [0,4,8], [2,4,6]
-        ]
-        for c in combinacoes:
-            if tabuleiro[c[0]] == tabuleiro[c[1]] == tabuleiro[c[2]] == jogador:
-                return True
-        return False
-    
-    def jogada_computador():
-        for i in range(9):
-            if tabuleiro[i] == " ":
-                tabuleiro[i] = "O"
-                if checar_vencedor("O"):
-                    return i
-                tabuleiro[i] = " "
-        
-        for i in range(9):
-            if tabuleiro[i] == " ":
-                tabuleiro[i] = "X"
-                if checar_vencedor("X"):
-                    tabuleiro[i] = "O"
-                    return i
-                tabuleiro[i] = " "
-        
-        if tabuleiro[4] == " ":
-            tabuleiro[4] = "O"
-            return 4
-        
-        cantos = [0,2,6,8]
-        random.shuffle(cantos)
-        for i in cantos:
-            if tabuleiro[i] == " ":
-                tabuleiro[i] = "O"
-                return i
-        
-        laterais = [1,3,5,7]
-        random.shuffle(laterais)
-        for i in laterais:
-            if tabuleiro[i] == " ":
-                tabuleiro[i] = "O"
-                return i
+    elif rodada == "0":
+        tabuleiro[linha][coluna] = "0"
+        validarVitoria(rodada)
+        Jogadas += 1
+        rodada = "X"
 
-    jogador_atual = "X"  
-    rodadas = 0
-
-    while rodadas < 9:
-        mostrar_tabuleiro()
-
-        if jogador_atual == "X":
-            pos = input("Escolha uma posição (1-9): ").strip()
-            if not pos.isdigit():
-                print("Digite um número de 1 a 9!")
-                continue
-            pos = int(pos) - 1
-            if pos < 0 or pos > 8:
-                print("Escolha inválida! Digite de 1 a 9.")
-                continue
-            if tabuleiro[pos] != " ":
-                print("Posição ocupada! Tente outra.")
-                continue
-            tabuleiro[pos] = "X"
-        else:
-            pos = jogada_computador()
-            print(f"Computador jogou na posição {pos + 1}")
-
-        rodadas += 1
-
-        if checar_vencedor(jogador_atual):
-            mostrar_tabuleiro()
-            if jogador_atual == "X":
-                print("Parabéns! Você venceu! 🏆")
-            else:
-                print("O computador venceu! 😢")
-            return
-
-        jogador_atual = "O" if jogador_atual == "X" else "X"
-
-    mostrar_tabuleiro()
-    print("Empate! 🤝")
-
-
-jogo_da_velha_inteligente()
+print("Fim de jogo!")
